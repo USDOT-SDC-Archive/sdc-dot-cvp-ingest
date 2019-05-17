7933bc1e9c3d127f6d2e5202926e2942a2f3a9b7
Debbie English <debbie@neaeraconsulting.com>
2019-05-17 16:48:19 -0600

    Added unit tests

--- dataProcessor-tests.py
+++ dataProcessor-tests.py
@@ -7,10 +7,9 @@ import boto3
 import os
 from moto import mock_s3
 
-
 class TestLambdaHandler(object):
     def setup(self):
-        json_event_data = """{
+        json_small_event_data = """{
           "Records":
           [
             {
@@ -26,8 +25,26 @@ class TestLambdaHandler(object):
             }
           ]
         }""".encode('utf-8')
-        self.event_data = json.loads(json_event_data)
+        self.samll_event_data = json.loads(json_small_event_data)
         
+        json_large_event_data = """{
+          "Records":
+          [
+            {
+              "s3": {
+                "bucket": {
+                  "name": "test"
+                },
+                "object": {
+                  "key": "bsm/file.csv",
+                  "size": 110000000
+                }
+              }
+            }
+          ]
+        }""".encode('utf-8')
+        self.large_event_data = json.loads(json_large_event_data)
+
         json_wrong_event_data = """{
           "Records":
           [
@@ -36,15 +53,16 @@ class TestLambdaHandler(object):
           ]
         }""".encode('utf-8')
         self.wrong_event_data = json.loads(json_wrong_event_data)
-        
+
     @mock_s3
     def test_lambda_handler_wydot(self):
         source_bucket = 'test'
         target_bucket = 'random_target_bucket'
         target_key = 'cv/wydot/'
         key = 'bsm/file.csv'
+        bodyfile = 'ola'
         
-        self.basic_template(source_bucket, target_bucket, target_key, key)
+        self.basic_template(source_bucket, target_bucket, target_key, key, bodyfile, self.small_event_data)
         
     @mock_s3
     def test_lambda_handler_thea(self):
@@ -52,11 +70,66 @@ class TestLambdaHandler(object):
         target_bucket = 'random_target_bucket'
         target_key = 'cv/thea/'
         key = 'bsm/file.csv'
-        
-        self.basic_template(source_bucket, target_bucket, target_key, key)
-        
+        bodyfile = 'ola'
+
+        self.basic_template(source_bucket, target_bucket, target_key, key, bodyfile, self.small_event_data)
+    
     @mock_s3
-    def basic_template(self, source_bucket, target_bucket, target_key, key):
+    def test_lambda_handler_with_wrong_event_data(self):
+        conn = boto3.resource('s3')
+        conn.create_bucket(Bucket='asd')
+
+        pytest.raises(KeyError)
+
+    @mock_s3
+    def test_lambda_handler_raise_exception(self):
+        source_bucket = 'test'
+        target_bucket = 'random_target_bucket'
+        target_key = 'cv/thea/'
+        key = 'bsm/file.csv'
+        bodyfile = 'ola'
+
+        self.error_template(source_bucket, target_bucket, target_key, key, bodyfile, self.large_event_data, True, True)
+
+    @mock_s3
+    def test_lambda_handler_no_environ_variable(self):
+        source_bucket = 'test'
+        target_bucket = 'random_target_bucket'
+        target_key = 'cv/thea/'
+        key = 'bsm/file.csv'
+        bodyfile = 'ola'
+
+        self.error_template(source_bucket, target_bucket, target_key, key, bodyfile, self.small_event_data, False, True)
+
+    @mock_s3
+    def test_lambda_handler_thea(self):
+        source_bucket = 'test'
+        target_bucket = 'wrong name'
+        target_key = 'cv/thea/'
+        key = 'bsm/file.csv'
+        bodyfile = 'ola'
+
+        self.error_template(source_bucket, target_bucket, target_key, key, bodyfile, self.small_event_data, True, False)
+
+    @mock_s3
+    def test_lambda_handler_large_file(self):
+        source_bucket = 'test'
+        target_bucket = 'random_target_bucket'
+        target_key = 'cv/thea/'
+        key = 'bsm/file.csv'
+        
+        f = open("110mbfile.txt", "w")
+        f.close()
+        os.truncate("110mbfile.txt", 110000000)
+
+        f = open("110mbfile.txt", "rb")
+        bodyfile = f
+        f.close()
+
+        self.basic_template(source_bucket, target_bucket, target_key, key, bodyfile, self.large_event_data)
+
+    @mock_s3
+    def basic_template(self, source_bucket, target_bucket, target_key, key, body, self_event_data):
         os.environ['TARGET_DATA_BUCKET'] = target_bucket
         os.environ['TARGET_DATA_KEY'] = target_key
 
@@ -67,14 +140,14 @@ class TestLambdaHandler(object):
         # Arrange
         # create object
         bucket = conn.Bucket(source_bucket)
-        bucket.put_object(Body='ola', Key=key)
+        bucket.put_object(Body=bodyfile, Key=key)
 
         bucket = conn.Bucket(target_bucket)
         count = len(list(bucket.objects.all()))
         assert count == 0, "Should be empty"
 
         # Act
-        dataProcessor.lambda_handler(self.event_data, '')
+        dataProcessor.lambda_handler(self_event_data, '')
 
         # Assert
         bucket = conn.Bucket(target_bucket)
@@ -87,9 +160,32 @@ class TestLambdaHandler(object):
 
         assert count == 1, "Should have new file"
 
-    @mock_s3
-    def test_lambda_handler_with_wrong_event_data(self):
-        conn = boto3.resource('s3')
-        conn.create_bucket(Bucket='asd')
+        bucket = conn.Bucket(source_bucket)
+        count = len(list(bucket.objects.all()))
+        assert count == 0, "Should be empty"
 
-        pytest.raises(KeyError)
+    @mock_s3
+    def error_template(self, source_bucket, target_bucket, target_key, key, body, self_event_data, define_target_bucket, correct_bucket):
+        if define_target_bucket:
+            os.environ['TARGET_DATA_KEY'] = target_key
+            os.environ['TARGET_DATA_BUCKET'] = target_bucket
+        if not correct_bucket:
+            target_bucket = 'purposefullyincorrectbucketname'
+        conn = boto3.resource('s3')
+        conn.create_bucket(Bucket=source_bucket)
+        conn.create_bucket(Bucket=target_bucket)
+
+        # Arrange
+        # create object
+        bucket = conn.Bucket(source_bucket)
+        bucket.put_object(Body=bodyfile, Key=key)
+
+        bucket = conn.Bucket(target_bucket)
+        count = len(list(bucket.objects.all()))
+        assert count == 0, "Should be empty"
+
+        # Act
+        try:
+            dataProcessor.lambda_handler(self_event_data, '')
+        except EntityTooSmall as e:
+            assert e is not None
