b8171fbf1660548670299674c31f093c8a8ae6c0
Aaron Franssell <aaron.franssell@gmail.com>
2020-07-31 10:32:13 -0400

    Terraform files (#12)
    * Terraform files
    
    * Fix casing on project

---
+++ terraform/data_provider_ingest_lambda.tf
@@ -0,0 +1,152 @@
+data "aws_s3_bucket_object" "lambda_zip" {
+  bucket = var.lambda_binary_bucket
+  key = "sdc-dot-cvp-ingest/data_processor.zip"
+}
+
+resource "aws_lambda_function" "IngestLambda" {
+    count = length(var.data_providers)
+    s3_bucket = data.aws_s3_bucket_object.lambda_zip.bucket
+    s3_key = data.aws_s3_bucket_object.lambda_zip.key
+    s3_object_version = data.aws_s3_bucket_object.lambda_zip.version_id
+    function_name = "${var.environment}-dot-sdc-${var.data_providers[count.index]["name"]}-manual-ingest"
+    role = aws_iam_role.IngestLambdaRole[count.index].arn
+    handler = "data_processor.lambda_handler"
+    runtime = "python3.7"
+    timeout = 900 # apparently some of these files get LARGE and can take a while to copy over
+    tags = merge({Name = var.data_providers[count.index]["ingest_bucket"],
+                  Team = var.data_providers[count.index]["team"],
+                  Project = var.data_providers[count.index]["project"]}, local.team_global_tags)
+    environment {
+        variables = {
+            TARGET_DATA_BUCKET = var.data_lake_bucket
+            BUCKET_PATH_MAPPING = jsonencode(map(var.data_providers[count.index]["ingest_bucket"], var.data_providers[count.index]["data_lake_destination"]))
+        }
+    }
+    vpc_config {
+        security_group_ids = [aws_security_group.lambda_http_egress.id]
+        subnet_ids = var.subnet_ids
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "IngestLambdaErrors" {
+  count = length(var.data_providers)
+  alarm_name = "${var.environment}-dot-sdc-${var.data_providers[count.index]["name"]}-manual-ingest-errors"
+  alarm_description = "Monitor errors for the ${var.environment}-dot-sdc-${var.data_providers[count.index]["name"]}-manual-ingest function"
+
+  namespace = "AWS/Lambda"
+  dimensions = {
+    FunctionName = "${var.environment}-dot-sdc-${var.data_providers[count.index]["name"]}-manual-ingest"
+  }
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold = "0"
+  evaluation_periods = "1"
+  metric_name = "Errors"
+  period = "60"
+  statistic = "Average"
+  
+  alarm_actions = var.lambda_error_actions
+}
+
+resource "aws_iam_role" "IngestLambdaRole" {
+  count = length(var.data_providers)
+  name = "${var.environment}-dot-sdc-${var.data_providers[count.index]["name"]}-manual-ingest"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${var.account_number}:root",
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+  EOF
+}
+
+resource "aws_iam_policy" "LambdaPermissions" {
+    count = length(var.data_providers)
+    name = "${var.environment}-dot-sdc-${var.data_providers[count.index]["name"]}-manual-ingest"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateNetworkInterface",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DeleteNetworkInterface",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVpcs"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": "logs:CreateLogGroup",
+      "Resource": "arn:aws:logs:${var.aws_region}:${var.account_number}:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+      ],
+      "Resource": [
+          "arn:aws:logs:${var.aws_region}:${var.account_number}:log-group:/aws/lambda/${var.environment}-dot-sdc-${var.data_providers[count.index]["name"]}-manual-ingest:*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:DeleteObject"
+      ],
+      "Resource": ["arn:aws:s3:::${aws_s3_bucket.data_provider_manual_ingest_bucket[count.index].bucket}/*", "arn:aws:s3:::${aws_s3_bucket.data_provider_manual_ingest_bucket[count.index].bucket}"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Resource": ["arn:aws:s3:::${var.data_lake_bucket}/*", "arn:aws:s3:::${var.data_lake_bucket}"]
+    }
+  ]
+}
+  EOF
+}
+
+resource "aws_iam_role_policy_attachment" "CloudWatchLogsAttachment" {
+    count = length(var.data_providers)
+    role = aws_iam_role.IngestLambdaRole[count.index].name
+    policy_arn = aws_iam_policy.LambdaPermissions[count.index].arn
+}
+
+resource "aws_lambda_permission" "AllowLambdaTriggerFromUpload" {
+    count = length(var.data_providers)
+    statement_id  = "AllowExecutionFromS3Bucket"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.IngestLambda[count.index].arn
+    principal = "s3.amazonaws.com"
+    source_arn = aws_s3_bucket.data_provider_manual_ingest_bucket[count.index].arn
+}
+
+resource "aws_s3_bucket_notification" "BucketNotification" {
+    count = length(var.data_providers)
+    bucket = aws_s3_bucket.data_provider_manual_ingest_bucket[count.index].id
+
+    lambda_function {
+        lambda_function_arn = aws_lambda_function.IngestLambda[count.index].arn
+        events = ["s3:ObjectCreated:*"]
+    }
+
+    depends_on = [aws_lambda_permission.AllowLambdaTriggerFromUpload]
+}
---
+++ terraform/input_variables.tf
@@ -0,0 +1,61 @@
+variable "environment" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "account_number" {
+  type = string
+}
+
+variable "lambda_binary_bucket" {
+  type = string
+  description = "The bucket that has the Lambda binary"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "The VPC to attach the Lambdas to"
+}
+
+variable "subnet_ids" {
+  type = list
+  description = "The subnets to attach the Lambda to"
+}
+
+variable "data_providers" {
+  type = list(object({
+    ingest_bucket = string
+    name = string
+    project = string
+    team = string
+    data_lake_destination = string
+  }))
+}
+
+variable "data_lake_bucket" {
+  type = string
+  description = "The name of the data lake S3 bucket where raw data resides"
+}
+
+variable "lambda_error_actions" {
+  type = list
+  description = "The list of SNS topics to send a notification to if the Lambdas throw an error"
+}
+
+locals {
+    team_global_tags = {
+        SourceRepo = "sdc-dot-cvp-ingest"
+        Environment = var.environment
+    }
+}
+
+locals {
+    global_tags = merge(local.team_global_tags, {
+        Project = "SDC-Platform"
+        Team = "sdc-platform"
+        Owner = "SDC support team"
+    })
+}
