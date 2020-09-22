339993a2348eb0a7a63e597cfe4cd09bc35495ad
Justin Spencer <justin.spencer@icf.com>
2020-09-22 18:14:51 -0400

    wip

--- firehose_replicator/firehose_replicator.py
+++ firehose_replicator/firehose_replicator.py
@@ -1,15 +1,42 @@
 import base64
 import boto3
 import os
+from datetime import datetime
+import uuid
 
 def lambda_handler(event, context):
     output = []
 
+    # TODO: split out the ARN part
+    stream_name = event['deliveryStreamArn']
+    print(f"Context is {context}")
+    print(f"Full event is {event}")
+
     for record in event['records']:
         print(f"Processing record {record['recordId']} ...")
+        
         file_content = base64.b64decode(record['data'])
+        timestamp_ns = record['approximateArrivalTimestamp']
+        timestamp_s = timestamp_ns / 1000
+        dt = datetime.fromtimestamp(timestamp_s)
+        
+        # 'deliveryStreamArn': 'arn:aws:firehose:us-east-1:911061262852:deliverystream/dev-dot-sdc-cvpep-wydot-alert'
+
+        # deliveryStreamArn is available as an attribute but should probably use env var
+        # e.g. cv/wydot/alert/
+        # key_prefix = os.environ.get('KEY_PREFIX')
+        key_prefix = 'cv/wydot/alert'
+        ymd_prefix = f"{dt.year}/{dt.month}/{dt.day}/"
+        key_name = f"{stream_name}-{dt.year}-{dt.month}-{dt.day}-{dt.hour}-{dt.minute}-{dt.second}-{uuid.uuid1()}.gz"
+        full_key = f"{key_prefix}/{ymd_prefix}/{key_name}"
 
         # TODO: push to ecs
+        # TODO: get timestamp?
+
+        # normal destination
+        # cv/wydot/alert/2020/09/22/21/dev-dot-sdc-cvpep-wydot-alert-8-2020-09-22-21-35-05-36dc0d56-9d8d-4e04-8a3d-4d034dc3fade.gz
+
+
         s3_client = boto3.client('s3')
         target_bucket = os.environ.get('ECS_BUCKET_NAME')
         # file_content = payload
